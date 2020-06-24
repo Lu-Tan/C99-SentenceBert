528a82d5a6a616d325baa1e2fa2a983553ff4250
LuTan <35918277+Lu-Tan@users.noreply.github.com>
2020-06-24 15:43:44 +0800

    read files, segment, log boundary points into file

--- basic embeddings.py
+++ basic embeddings.py
@@ -1,13 +1,23 @@
 """
-This basic example loads a pre-trained model from the web and uses it to
-generate sentence embeddings for a given list of sentences.
-"""
+2020/06/24
+System: ubuntu 18.04
+Python 3.6
+Additional import: sentence bert
 
+Function: Segment files into 3 segments and log boundary positions into file.
+"""
 from sentence_transformers import SentenceTransformer, LoggingHandler
 import numpy as np
 import logging
-import sys
 
+
+def initialize():
+    # Just some code to print debug information to stdout
+    np.set_printoptions(threshold=100)
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO,
+                        handlers=[LoggingHandler()])
 
 def cal_Euclidean_sim_matrix(embeddings):
     """
@@ -52,6 +62,7 @@ def cal_cos_sim_matrix(embeddings):
             sim_matrix[j, i] = sim_matrix[i, j]
     return sim_matrix
 
+
 def cal_rank(matrix, length, N):
     ranked_matrix = np.zeros((length, length))
     # scan all values in the matrix
@@ -72,25 +83,24 @@ def cal_rank(matrix, length, N):
             # print("ranked_matrix[" + str(i) + "," + str(j) + "] = " + str(_num_lower) + "/" + str(_num_examined))
     return ranked_matrix
 
+
 def s(ranked_matrix, start, end):
     _sum = 0
     for i in range(start, end):
         for j in range(start, end):
             _sum += ranked_matrix[i, j]
     return _sum
+
+
 def a(start, end):
     return (end-start + 1) ** 2
-def D(matrix, end, i):
+
+
+def D(ranked_matrix, end, i):
     sum_sk = s(ranked_matrix, 1, i) + s(ranked_matrix, i, end)
     sum_ak = a(1, i) + a(i + 1, end)
     temp = sum_sk / sum_ak
     return temp
-
-
-# def cal_m(matrix, length):
-#     d_n = []
-#     for i in range(1, length):
-#         _temp =
 
 
 def cluster(ranked_matrix, start, end):
@@ -104,71 +114,64 @@ def cluster(ranked_matrix, start, end):
         if temp > max:
             max = temp
             point_2 = i
-    print("half point is: " + str(point_2) + ", max value of D is " + str(max))
-    print("Log: " + str(log))
+    # print("half point is: " + str(point_2) + ", max value of D is " + str(max))
+    # print("Log: " + str(log))
     return point_2  # return boundary
 
 
-# Just some code to print debug information to stdout
-np.set_printoptions(threshold=100)
-logging.basicConfig(format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO,
-                    handlers=[LoggingHandler()])
-# /print debug information to stdout
+def log_result(result):
+    file_result = open("file_result.txt", "a")
+    file_result.write(str(result) + '\n')
+    file_result.close()
 
 
-# Load Sentence model (based on BERT) from URL
-model = SentenceTransformer('bert-base-nli-mean-tokens')
+def segmente_one_file(model, filename):
+    # read sentences in a file and calculate its embeddings[]
+    sentences = []
+    with open(filename, 'r') as f:
+        for line in f:
+            sentence = "".join(line.strip('\n').split(','))  # Convert list to string
+            sentence = sentence[3:]  # delete speaker and tab
+            sentences.append(sentence)
+    sentence_embeddings = model.encode(sentences)
+    embeddings = []
+    # The result is a list of sentence embeddings as numpy arrays
+    for sentence, embedding in zip(sentences, sentence_embeddings):
+        embeddings.append(embedding)
 
-sentences = []
-with open('high_001', 'r') as f:
-    for line in f:
-        sentence = "".join(line.strip('\n').split(','))  # Convert list to string
-        sentence = sentence[3:]  # delete speaker and tab
-        sentences.append(sentence)
-# print(sentences)
-# Embed a list of sentences
-# sentences = ['This framework generates embeddings for each input sentence',
-#              'Sentences are passed as a list of string.',
-#              'The quick brown fox jumps over the lazy dog.']
-sentence_embeddings = model.encode(sentences)
+    # calculate ranked_matrix from embeddings[]
+    ranked_matrix = cal_rank(cal_cos_sim_matrix(embeddings), len(embeddings), 11)
+    divide_into_2 = cluster(ranked_matrix, 0, len(embeddings))
+    if divide_into_2 - 0 > len(embeddings) - divide_into_2:
+        divide_into_3 = cluster(ranked_matrix, 0, divide_into_2)
+    else:
+        divide_into_3 = cluster(ranked_matrix, divide_into_2, len(embeddings))
 
-embeddings = []
-# The result is a list of sentence embeddings as numpy arrays
-for sentence, embedding in zip(sentences, sentence_embeddings):
-    # print("Sentence:", sentence)
-    # print("Embedding:", embedding)
-    embeddings.append(embedding)
-    # print("")
+    # log result[] as boundaries
+    result = []
+    if divide_into_2 > divide_into_3:
+        result.append(divide_into_3)
+        result.append(divide_into_2)
+    else:
+        result.append(divide_into_2)
+        result.append(divide_into_3)
+    # print("divide result is: " + str(result))
 
-# vec1 = np.array(embeddings[0])
-# vec2 = np.array(embeddings[1])
-# distance = np.sqrt(np.sum(np.square(vec1 - vec2)))
-# print("distance: " + str(distance))
-# print("cal_sim_matrix(embeddings)")
-# print(cal_cos_sim_matrix(embeddings))
+    # log boundaries of curret file into "file_result.txt"
+    log_result(result)
+    print("Boundary points of " + filename + " are " + str(result))
 
-ranked_matrix = cal_rank(cal_cos_sim_matrix(embeddings), len(embeddings), 11)
-# print("ranked matrix: ")
-# print(ranked_matrix)
-divide_into_2 = cluster(ranked_matrix, 0, len(embeddings))
-if divide_into_2 - 0 > len(embeddings) - divide_into_2:
-    divide_into_3 = cluster(ranked_matrix, 0, divide_into_2)
-else:
-    divide_into_3 = cluster(ranked_matrix, divide_into_2, len(embeddings))
 
-result = []
-if divide_into_2 > divide_into_3 :
-    result.append(divide_into_3)
-    result.append(divide_into_2)
-else:
-    result.append(divide_into_2)
-    result.append(divide_into_3)
-print("divide result is: " + str(result))
-# cluster(ranked_matrix, 4, len(embeddings))
-# cluster(ranked_matrix, 4, 15)
-# cluster(ranked_matrix, 5, 15)
-# cluster(ranked_matrix, 7, 15)
+def main():
+    initialize()
+    # Load Sentence model (based on BERT) from URL
+    model = SentenceTransformer('bert-base-nli-mean-tokens')
+    for i in range(1, 154):
+        i = '%03d' % i
+        filename = 'raw_conversation/' + 'high_' + str(i)
+        # print(filename)
+        segmente_one_file(model, filename)
 
+
+main()
 
